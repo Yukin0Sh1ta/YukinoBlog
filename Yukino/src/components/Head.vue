e9a8228d836199b785d4913170b3d7b0af9f8f6a
麻辣猪猪侠 <12952572+q360874368@user.noreply.gitee.com>
2026-07-08 16:29:36 +0800

    正式提交

--- Yukino/src/components/Head.vue
+++ Yukino/src/components/Head.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex fixed items-center h-[12%] w-full bg-black top-0 left-0 z-1000">
+  <div class="flex fixed items-center h-[12%] w-full bg-black top-0 left-0 z-[1000]">
     <div
-      class="ml-25 select-none cursor-pointer text-[22px] text-[#9a9a9a]"
+      class="ml-[100px] select-none cursor-pointer text-[22px] text-[#9a9a9a]"
       @click="home"
     >首页</div>
     <div
-      class="ml-25 select-none cursor-pointer text-[22px] text-[#9a9a9a]"
+      class="ml-[100px] select-none cursor-pointer text-[22px] text-[#9a9a9a]"
       @click="about"
     >文章</div>
     <div
-      class="ml-25 select-none cursor-pointer text-[22px] text-[#9a9a9a]"
+      class="ml-[100px] select-none cursor-pointer text-[22px] text-[#9a9a9a]"
       @click="talk"
     >树洞</div>
     <div class="ml-auto mr-10 select-none text-xl text-[#9a9a9a] italic">Yukino's blog</div>
